f0ce1c7a46e2af4768d7a30f0b1019f057167e74
Matias Borghi <borghi.matias@gmail.com>
2023-06-16 12:20:13 -0300

    create deal function that returns a handleSize number of cards

--- 02_cards/main.go
+++ 02_cards/main.go
@@ -1,6 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-	cards.print()
+	hand, remainingCards := deal(cards, 5)
+
+	fmt.Println("Hand: ")
+	hand.print()
+	fmt.Println("Remaining Cards: ")
+	remainingCards.print()
 }
